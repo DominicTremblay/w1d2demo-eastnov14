cae17d462cb4fa986a125e90f9da0f6125892453
Dominic Tremblay <dominictremblay24@gmail.com>
2022-11-15 12:31:35 -0500

    final version of our demo

--- sum.js
+++ sum.js
@@ -4,6 +4,10 @@
 const args = process.argv.slice(2);
 console.log('arguments', args);
 // Edge case: We need at least 2 arguments.
+if (args.length < 2) {
+  console.log(`Error, please enter at least 2 arguments`);
+  process.exit();
+} 
 
 // we need an accumulator
 
@@ -11,15 +15,31 @@ let total = 0;
 
 // loop through each
 for (let arg of args) {
-  // add them together
-  // Typecast the string into a number
-  total += Number(arg);
+  // Edge case: If any argument is not a whole number, skip it.
+  // Math.floor
+  // typeof?
+  // % modulo => arg % 1 === 0
+  // regex
+  // => Number.isInteger()
 
-  console.log('arg:', arg, 'total:', total, "type:", typeof Number(arg));
+  if (Number.isInteger(Number(arg))) {
+    // add them together
+    // Typecast the string into a number
+    total += Number(arg);
+  }
+
+  // Edge case: If any argument is not a number, output an error message.
+  if(isNaN(Number(arg))) {
+    console.log(`Error: please enter only numbers`);
+    // stop the execution
+    // return;
+    process.exit();
+  } 
+  // console.log('arg:', arg, 'total:', total, "type:", typeof Number(arg));
+    // print out the sum
+    console.log('total', total);
+
+
 }
 
-// Edge case: If any argument is not a whole number, skip it.
-// Edge case: If any argument is not a number, output an error message.
 
-// print out the sum
-console.log('total', total);
